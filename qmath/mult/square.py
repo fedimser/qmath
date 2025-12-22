8d06de5d761d2eed5160a2c1a4df33f18ece50b5
Dmytro Fedoriaka <fedimser@uw.edu>
2025-12-22 06:03:38 +0000

    implement square

--- qmath/mult/square.py
+++ qmath/mult/square.py
@@ -1,42 +1,69 @@
 import numpy as np
 import psiqworkbench.qubricks as qbk
-from psiqworkbench import QFixed, QUInt, Qubits
+from psiqworkbench import QFixed, QUFixed, QUInt, Qubits
 from psiqworkbench.qubricks import Qubrick
 
 from ..utils.gates import parallel_cnot
 
 
+class AbsInPlace(Qubrick):
+    """Computes absolute value."""
+
+    def _compute(self, x: QFixed):
+        sign = self.alloc_temp_qreg(1, "sign")
+        sign.lelbow(x[-1])
+        x.x(sign)
+        qbk.GidneyAdd().compute(QUInt(x), 1, sign)
+
+
 class SquareIteration(Qubrick):
-    def _compute(self, a: Qubits, b: Qubits):
-        b[0].x(a[0])
-        i_max = min(a.num_qubits - 1, b.num_qubits - 2)
-        for i in range(1, i_max + 1):
-            # TODO: replace with Gidney Elbows.
-            b[i + 1].x(a[0] | a[i])
+    def _compute(self, x: Qubits, anc: Qubits, i, j, skip):
+        if skip == 0:
+            # print(f"x[{i}]^2 -> ans[{j}]")
+            anc[j].x(x[i])
+        for ii in range(max(1, skip - 1), 1000):
+            anc_pos = j + (ii - skip + 1)
+            if (i + ii) >= x.num_qubits or anc_pos >= anc.num_qubits:
+                break
+            assert 0 <= anc_pos < anc.num_qubits
+            # print(f"x[{i}]*x[{i+ii}] -> ans[{anc_pos}]")
+            anc[anc_pos].x(x[i] | x[i + ii])
 
 
-# TODO: make this more efficient.
-# We don't need extra register for copy. See how psiqworkbench.qubricks.Square is implemented.
 class Square(Qubrick):
     """Computes square of given QFixed register."""
 
-    # TODO: handle sign (if negative, do abs)!
+    def _compute_unsigned(self, x: QUFixed, target: QUFixed):
+        """Computes square assuming x is unsigned.
 
-    def _compute_new(self, x: QFixed, target: QFixed) -> QFixed:
-        anc = self.alloc_temp_qreg(target.num_qubits, "anc")
-        for i in range(x.num_qubits):
-            # Where in target to write x[i]^2.
-            j = 2 * (i - x.radix) + target.radix
+        Uses algorithm from Lemma 6 in https://arxiv.org/pdf/2105.12767.
+        """
+        anc: Qubits = self.alloc_temp_qreg(target.num_qubits, "anc")
+        for i in range(x.num_qubits - 1):
+            j = 2 * (i - x.radix) + target.radix  # Where in target to write x[i]^2.
             if j >= target.num_qubits:
                 break
-            with SquareIteration().computed(x[i:], anc[j:]):
-                qbk.GidneyAdd().compute(target[j:], anc[j:])
+            skip = 0
+            if j < 0:
+                skip = -j
+                j = 0
 
-    def _compute(self, x: QFixed, target: QFixed) -> QFixed:
-        x_reg = Qubits(x)
-        x_copy_reg: Qubits = self.alloc_temp_qreg(x.num_qubits, "x_copy")
-        x_copy = QFixed(x_copy_reg, radix=x.radix)
-        parallel_cnot(x_reg, x_copy_reg)
-        qbk.GidneyMultiplyAdd().compute(target, x, x_copy)
-        parallel_cnot(x_reg, x_copy_reg)
-        x_copy_reg.release()
+            with SquareIteration().computed(x, anc, i, j, skip):
+                qbk.GidneyAdd().compute(target[j:], anc[j:])
+        anc.release()
+
+    def _compute(self, x: QFixed, target: QFixed, fallback_to_mul=True):
+        if fallback_to_mul:
+            x_reg = Qubits(x)
+            x_copy_reg: Qubits = self.alloc_temp_qreg(x.num_qubits, "x_copy")
+            x_copy = QFixed(x_copy_reg, radix=x.radix)
+            parallel_cnot(x_reg, x_copy_reg)
+            qbk.GidneyMultiplyAdd().compute(target, x, x_copy)
+            parallel_cnot(x_reg, x_copy_reg)
+            x_copy_reg.release()
+            return
+
+        with AbsInPlace().computed(x):
+            x_unsigned = QUFixed(x[0 : x.num_qubits - 1], radix=x.radix)
+            target_unsigned = QUFixed(target[0 : target.num_qubits - 1], radix=target.radix)
+            self._compute_unsigned(x_unsigned, target_unsigned)
--- qmath/mult/square_test.py
+++ qmath/mult/square_test.py
@@ -5,12 +5,34 @@ import pytest
 
 from qmath.mult import Square
 from qmath.utils.test_utils import QPUTestHelper
+from psiqworkbench.filter_presets import BIT_DEFAULT
+from psiqworkbench import QPU, Qubits, QFixed, QUInt
 
 RUN_SLOW_TESTS = os.getenv("RUN_SLOW_TESTS") == "1"
 
 
-def test_square():
-    qpu_helper = QPUTestHelper(num_qubits=200, qubits_per_reg=25, radix=17, num_inputs=2)
+def test_square_5bit():
+    qpu = QPU(filters=BIT_DEFAULT)
+    qpu.reset(25)
+    q_x = QFixed(5, name="x", radix=2, qpu=qpu)
+    q_ans = QFixed(5, name="ans", radix=2, qpu=qpu)
+    q_x.write(1.5)
+    Square().compute(q_x, q_ans)
+    assert q_ans.read() == 2.25
+
+
+def test_square_8bit():
+    qpu = QPU(filters=BIT_DEFAULT)
+    qpu.reset(100)
+    q_x = QFixed(8, name="x", radix=4, qpu=qpu)
+    q_ans = QFixed(8, name="ans", radix=4, qpu=qpu)
+    q_x.write(1.5)
+    Square().compute(q_x, q_ans)
+    assert q_ans.read() == 2.25
+
+
+def test_square_random():
+    qpu_helper = QPUTestHelper(num_qubits=200, qubits_per_reg=26, radix=18, num_inputs=2)
     q_x, q_ans = qpu_helper.inputs
     Square().compute(q_x, q_ans)
     qpu_helper.record_op(q_ans)
@@ -22,13 +44,13 @@ def test_square():
 
 
 @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="slow test")
-def test_square_high_precision():
-    qpu_helper = QPUTestHelper(num_qubits=500, qubits_per_reg=50, radix=40, num_inputs=2)
+def test_square_random_high_precision():
+    qpu_helper = QPUTestHelper(num_qubits=500, qubits_per_reg=51, radix=41, num_inputs=2)
     q_x, q_ans = qpu_helper.inputs
     Square().compute(q_x, q_ans)
     qpu_helper.record_op(q_ans)
 
     for _ in range(50):
         x = -10 + 20 * random.random()
-        result = qpu_helper.apply_op([x, 0])
+        result = qpu_helper.apply_op([x, 0], check_no_side_effect=True)
         assert abs(result - x**2) < 1e-11
